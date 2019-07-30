ae1c5b62dfe53593147ecd357eb8bef7f12404f7
Crazycyborg1995 <mhdafsalvp@gmail.com>
2019-07-30 12:59:34 +0530

    Updates

---
+++ precache-manifest.85e91e7722450f08aca1f65cdcdaa788.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d2ca3f7e95a3319bead1f854a681ebd",
+    "url": "/bank-search-app/index.html"
+  },
+  {
+    "revision": "2acabed091cca0779dfc",
+    "url": "/bank-search-app/static/css/main.73b31b1d.chunk.css"
+  },
+  {
+    "revision": "9739ab37bd6c90e1837d",
+    "url": "/bank-search-app/static/js/2.7d3f09cc.chunk.js"
+  },
+  {
+    "revision": "2acabed091cca0779dfc",
+    "url": "/bank-search-app/static/js/main.59ee1fd4.chunk.js"
+  },
+  {
+    "revision": "d311bf913346af743ee9",
+    "url": "/bank-search-app/static/js/runtime~main.c95ac1ed.js"
+  },
+  {
+    "revision": "aa7b03b3798417de76f0b62ab4d1c00e",
+    "url": "/bank-search-app/static/media/heart.aa7b03b3.svg"
+  },
+  {
+    "revision": "d654ce194e0660d047d6913124f8d562",
+    "url": "/bank-search-app/static/media/like.d654ce19.svg"
+  }
+]);
